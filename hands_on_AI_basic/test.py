de82f38e0215b0aa9e8d7dcc483abe6bc70712fd
Anuj Shah <anujonline645@gmail.com>
2018-11-07 13:32:43 +0530

    python  notebook for hands on

---
+++ hands_on_AI_basic/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib
+
+import tensorflow as tf
+import keras
+import cv2
+import sklearn
+
+print ('installed numpy: ', np.__version__)
+print ('installed pandas: ', pd.__version__)
+print ('installed seaborn: ', sns.__version__)
+print ('installed matplotlib: ', matplotlib.__version__)
+print ('installed tensorflow: ', tf.__version__)
+print ('installed keras: ', keras.__version__)
+print ('installed sklearn: ', sklearn.__version__)
